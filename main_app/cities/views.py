f0f1b6dced08ea0bf318adf81e385835ae8dc5ee
Daniil_Boika <daniilboiko99@gmail.com>
2022-09-24 16:28:56 +0200

    added first config with cities

--- main_app/cities/views.py
+++ main_app/cities/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import City
 
 # Create your views here.
 
 
-def test(request):
-    return HttpResponse('first test')
+# def test(request):
+#     return HttpResponse('first test')
+
+def cities_showcase(request):
+    cities = City.objects.all()
+    context = {'context': cities}
+    return render(request, 'cities/index.html', context)
