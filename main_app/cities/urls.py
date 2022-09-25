7290e97e7fec7efac18cd52c5fca81f018a1d610
Daniil_Boika <daniilboiko99@gmail.com>
2022-09-25 19:02:08 +0200

    detail view for cities is configured

--- main_app/cities/urls.py
+++ main_app/cities/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.cities_showcase, name='first_test'),
+    path('<int:id>/', views.cities_showcase_detail, name='detail_view'),
 ]
--- main_app/cities/views.py
+++ main_app/cities/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import City
 
@@ -8,7 +8,14 @@ from .models import City
 # def test(request):
 #     return HttpResponse('first test')
 
+def cities_showcase_detail(request, id):
+    cities = get_object_or_404(City, id=id)
+    context = {'context': cities}
+    return render(request, 'cities/detail_view.html', context)
+
+
 def cities_showcase(request):
     cities = City.objects.all()
     context = {'context': cities}
     return render(request, 'cities/index.html', context)
+
