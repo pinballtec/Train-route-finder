0cbfd31c1c1efbb6c436ed1ebbdf7f4af983cff7
Daniil_Boika <daniilboiko99@gmail.com>
2022-12-03 21:21:13 +0100

    added treatment for serch func

--- main_app/rotes/utils.py
+++ main_app/rotes/utils.py
@@ -1,12 +1,48 @@
 from trains.models import Trains
 
+def dfs_paths(graph, start, goal):
+    """
+    function for search of graphs with routes
+    """
+    stack = [(start, [start])]
+    while stack:
+        (vertex, path) = stack.pop()
+        if vertex in graph.keys():
+            for next_ in graph[vertex] - set(path):
+                if next_ == goal:
+                    yield path + [next_]
+                else:
+                    stack.append((next_, path + [next_]))
+
+
 def get_graph(qs):
     graph = {}
     for i in qs:
-        graph.setdefault(i.from_city_id, set())
-        graph[i.from_city_id].add(i.to_city_id)
+        graph.setdefault(i.arrival_city_id, set())
+        graph[i.arrival_city_id].add(i.departure_city_id)
     return(graph)
 
+
 def get_routes(request, form) -> dict:
+    context = {'form': form}
     qs = Trains.objects.all()
     graph = get_graph(qs)
+    "cleaning method for form"
+    data = form.cleaned_data
+    from_city = data['from_city']
+    to_city = data['to_city']
+    traveling_time = data['traveling_time']
+    cities = data['cities']
+    all_ways = list(dfs_paths(graph, from_city.id, to_city.id))
+    if not len(all_ways):
+        raise ValueError('Route not found')
+    if cities:
+        _cities = [city.id for city in _cities]
+        right_ways = []
+        for route in all_ways:
+            if all(city in route for city in _cities):
+                right_ways.append(route)
+            if not right_ways:
+                raise ValueError('This Route is not possible')
+    return context
+
